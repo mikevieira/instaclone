015fe90457dd07dda387ccb2e4a801c6d8db40ef
Mike Vieira <mikevieira@hotmail.com>
2017-01-31 20:11:55 +0000

    Authentication tests added

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'simple_form'
+gem 'devise'
 
 
 group :development, :test do
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,6 @@
 class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
 
 def index
 end
@@ -10,8 +12,13 @@ end
 
 
 def create
-  @gram = Gram.create(gram_params)
-  redirect_to root_path
+  @gram = current_user.grams.create(gram_params)
+  if @gram.valid?
+     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+
 end
 
 
