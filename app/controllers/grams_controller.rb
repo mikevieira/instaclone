b6700cdd56980a7450a0e21df314d7de916a97e6
Mike Vieira <mikevieira@hotmail.com>
2017-02-01 15:42:50 +0000

    Show and show tests hooked up

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,6 +5,14 @@ class GramsController < ApplicationController
 def index
 end
 
+def show
+  @gram =Gram.find_by_id(params[:id])
+  if @gram.blank?
+    render plain: 'Not Found :(', status: :not_found
+  end
+end
+
+
 
 def new
   @gram = Gram.new
